d8e807a9fa8923e92e5236cf845316b1733851d3
w123640a <w123640a@gmail.com>
2021-07-22 15:24:30 +0800

    數字

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -23,7 +23,7 @@ namespace WindowsFormsApp1
         private void button1_Click(object sender, EventArgs e)
         {
             MessageBox.Show("test");
-
+            MessageBox.Show("1234");
 
         }
     }
